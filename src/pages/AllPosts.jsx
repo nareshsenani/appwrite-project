76e9ab00cbe58a4505ace2e88039b62265499419
naresh senani <senaninaresh@gmail.com>
2025-04-11 18:27:51 +0530

    updated file content

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -6,13 +6,16 @@ import appwriteService from "../appwrite/config"
 function AllPosts() {
     const [posts, setPosts] = useState([])
     
-    useEffect(() => {}, [])
+    useEffect(() => {
+        appwriteService.getPosts([]).then((posts) => {
+            if (posts) {
+                console.log(posts.documents)
+                setPosts(posts.documents)
+            } 
+        })
+    }, [])
 
-    appwriteService.getPosts([]).then((posts) => {
-        if (posts) {
-            setPosts(posts.documents)
-        } 
-    })
+    
    
     return (
         <div className=' w-full py-8'>
