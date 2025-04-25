068381ad175008b743f115cc08a7c8a9ca7f1c9b
naresh senani <senaninaresh@gmail.com>
2025-04-25 13:22:20 +0530

    updated file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -39,19 +39,19 @@ function Header() {
    <header className='py-3 shadow bg-gray-500' >
     <Container>
       <nav className='flex'>
-        <div className='mr-4'>
+        <div className='w-[100px] px-2 flex items-center  md:w-1/4 lg:w-1/5'>
           <Link to={'/'}>
-          <Logo width='70px'/>
+          <Logo noPadding width='100%'/>
           </Link>
         </div>
 
-        <ul className='flex ml-auto'>
+        <ul className='flex ml-auto flex-shrink'>
           {navItems.map((item) => 
           item.active ? (
             <li key={item.name}>
               <button 
               onClick={() => navigate(item.slug)}
-              className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+              className='inline-block px-3 sm:px-4 md:px-5 py-2 duration-200 hover:bg-blue-100 rounded-full'
               >{item.name}
               </button>
             </li>
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-function Logo({width ='100px'}) {
+function Logo({width ='100px',noPadding = false,}) {
   return (
-    <div>
-      Logo
+    <div className={`w-full flex justify-start items-center ${noPadding ? '' : 'px-4 py-2'} ${width}`}>
+
+      <img className='w-1/3 max-w-[150px] rounded-lg md:w-1/4 lg:w-1/5' 
+      src="src/assets/blog.png"
+      alt="" />
     </div>
   )
 }
