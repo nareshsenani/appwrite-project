54cdf1f528e3d1f7b1d32c388122471476c7a409
naresh senani <senaninaresh@gmail.com>
2025-04-25 13:48:52 +0530

    updated file

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,7 +5,7 @@ function Logo({width ='100px',noPadding = false,}) {
     <div className={`w-full flex justify-start items-center ${noPadding ? '' : 'px-4 py-2'} ${width}`}>
 
       <img className='w-1/3 max-w-[150px] rounded-lg md:w-1/4 lg:w-1/5' 
-      src="public/assets/blog.png"
+      src="/assets/blog.png"
       alt="" />
     </div>
   )
