908ab40d823d0a4ac82edbc6495e71355e38e433
naresh senani <senaninaresh@gmail.com>
2025-07-03 09:51:46 +0530

    Fix: updated image path and made layout responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import appwriteService from '../appwrite/config'
 import { Container, PostCard } from '../components'
+import { Link } from 'react-router-dom'
 
 
 
@@ -8,25 +9,35 @@ function Home() {
 
     const [posts, setPosts] = useState([])
 
-   
+
     useEffect(() => {
-       appwriteService.getPosts().then((posts) => {
-        if(posts){
-            setPosts(posts.documents)
-        }
-       })
+        appwriteService.getPosts().then((posts) => {
+            if (posts) {
+                setPosts(posts.documents)
+            }
+        })
     }, [])
 
     if (posts.length === 0) {
         return (
-            <div className='w-full py-8 mt-4 text-center '>
+            <div className='w-full py-8 text-center'>
                 <Container>
-                    <div className='flex flex-wrap'>
-                        <div className='p-2 w-full'>
-                            <h1 className='text-2xl'>
-                                Login to read posts
-                            </h1>
-                        </div>
+                    <div className='flex justify-center items-center flex-col '>
+                        <h1 className='text-4xl font-bold mb-4'>Welcome to MyBlog!</h1>
+                        <p className='text-lg text-gray-600 mb-6'>Discover amazing articles written by awesome developers. Login to explore!</p>
+                        <img
+                            src='/assets/login-illustration.svg'
+                            alt='Login Illustration'
+                            className='w-64 sm:w-72 md:w-80 lg:w-96 mb-6'
+                        />
+                        <Link to='/login'
+                            className='inline-block px-6 py-3 bg-blue-600 text-white rounded-lg text-lg hover:bg-blue-700 transition'
+
+                        >
+                            Login to Read Posts
+                        </Link>
+
+
                     </div>
                 </Container>
             </div>
